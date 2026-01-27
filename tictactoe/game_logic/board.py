ec06908cacff539275d3ad507473c9f8734b12b8
danielgil666 <danigildardo666@gmail.com>
2026-01-26 18:51:43 -0700

    continuacion de game_logic.py

--- tictactoe/game_logic/board.py
+++ tictactoe/game_logic/board.py
@@ -14,13 +14,33 @@ def display_board(dboard:dict) -> None:
     print("--+--+--")
     print(f"{d[6]:2s}|{d[7]:2}|{d[8]:2s}")
 
+
+def player_turn(player:str, dboard:dict)->bool:
+
+    user_input = input(f"Player{player},Enter your move(0-8):")
+    user_input = int(user_input) 
+    print(f"value entered: {user_input} type: {type(user_input)}")
+    if user_input in dboard.keys():
+        if dboard[user_input] not in ['x','0']:
+            dboard[user_input] = player
+            valid_move = True
+        else:
+            print("Invalid move: cell already occupied.")
+    else:
+            print("invalid move: cell doesn't exist")
+    return valid_move
+
 if __name__ == "__main__":
-    board = {x:str(x) for x in range(9)}
-    board[0] = "X"
-    board[4] = "O"
-    display_board(board)
-    
-    
+     board = {x:str(x) for x in range(9)}
+     display_board(board)
+     move=player_turn('X',board)
+     print()
+
+    # board[0] = "X"
+    # board[4] = "O"
+     display_board(board)
+     player_turn("X", board)
+     print(board)
     
      # Tictactoe
 
---
+++ tictactoe/game_logic/game_logic.py
@@ -0,0 +1,54 @@
+
+
+
+"""
+Author:Daniel Gil Cota
+Here goes the game logic for Tictactoe
+"""
+import board
+
+def check_winner(d:dict, combo_list:list)->bool:
+    """
+    Check if there is a winner
+    """
+    for combo in combo_list:
+        if d[combo[0]] == d[combo[1]] == d[combo[2]]:
+            return True
+    return False
+def game():
+    """
+    Here lives the main game logic
+    """
+    turn = 0
+    dboard = {x:str(x) for x in range(9)}
+    combo_list = [
+        [0,1,2], [3,4,5], [6,7,8], # rows
+        [0,3,6], [1,4,7], [2,5,8], # columns
+        [0,4,8], [2,4,6]           # diagonals
+    ]
+    x_player = 'X'
+    o_player = 'O'
+    current_player = x_player
+    winner=False
+    w_player
+    while turn < 9:
+        board.display_board(dboard)
+        valid_move = False
+        while not valid_move:
+            valid_move = board.player_turn(current_player, dboard)
+        turn += 1
+        w= check_winner(dboard,combo_list)
+        if winner:
+            w_player=current_player
+        if current_player == x_player:
+            current_player = o_player
+        else:
+            current_player = x_player
+            board.display_board(board)
+            if winner:
+                    print(f"Winner:Player{w_player}")
+            else:
+                    print(f"It's a tie!")
+
+if __name__ == "__main__":
+    game()
